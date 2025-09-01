ebfc4844e6e556bdb463ea9aa79e662b40d514e4
atul <atulkumar5301@gmail.com>
2025-09-01 22:18:02 +0530

    practice: create payment_gateway_interface_prac.go and methods_prac.go

--- src/payment_gateway_interface_prac.go
+++ src/payment_gateway_interface_prac.go
@@ -2,28 +2,32 @@ package main
 
 import "fmt"
 
-// Job description
-type Payment interface {
-    Pay(amount float64)
-}
 
-// PayPal
-type PayPal struct{}
-func (PayPal) Pay(amount float64) {
-    fmt.Printf("Paid %.2f using PayPal\n", amount)
-}
-
-// CreditCard
-type CreditCard struct{}
-func (CreditCard) Pay(amount float64) {
-    fmt.Printf("Paid %.2f using CreditCard\n", amount)
-}
-
-func Checkout(p Payment, amount float64) {
-    p.Pay(amount)
-}
 
 func main() {
     Checkout(PayPal{}, 99.99)
     Checkout(CreditCard{}, 49.50)
+}
+
+type Payment interface {
+    Pay(amount float64)
+}
+
+
+type PayPal struct{}
+func (PayPal) Pay(amount float64) {
+
+    fmt.Printf("Paid %f using PayPal\n", amount)
+}
+
+type CreditCard struct{}
+
+func (CreditCard) Pay(amount float64) {
+    
+    fmt.Printf("Paid %f using CreditCard\n", amount)
+}
+
+func Checkout(p Payment, amount float64) {
+
+    p.Pay(amount)
 }
